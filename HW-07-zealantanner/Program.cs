d3f5f71a0a64f0e2e927460b3e7de7959010b28a
zealantanner <77339919+zealantanner@users.noreply.github.com>
2021-02-02 01:20:46 -0700

    Add files via upload

---
+++ HW-07-zealantanner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+interface IPoint
+{
+    int X {get; set;}
+    int Y {get; set;}
+}
+
+class Point : IPoint
+{
+    public Point(int newX, int newY)
+    {
+        X = newX;
+        Y = newY;
+    }
+    public int X {get; set;}
+    public int Y {get; set;}
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        IPoint aPoint = new Point(10,10);
+        Console.WriteLine($"The Point: ({aPoint.X},{aPoint.Y})");
+    }
+}
